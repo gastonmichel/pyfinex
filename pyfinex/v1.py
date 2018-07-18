06fd4864482ef999d5677eac4ac94196f5cbb02f
Gastón Michel <michel.z.gaston@gmail.com>
2018-07-18 15:54:03 -0300

    Minor changes in v1 regarding GET pathparams

--- pyfinex/v1.py
+++ pyfinex/v1.py
@@ -1,6 +1,3 @@
-# Import Built-Ins
-import json
-
 # Import Homebrew
 from pyfinex.api import API
 
@@ -12,50 +9,44 @@ class V1(API):
     # REST PUBLIC ENDPOINTS ###################################################
     def ticker(self, **params):
         """ Gives innermost bid and asks and information on the most recent trade, as well as high, low and volume of the last 24 hours.
-        Docs: https://docs.bitfinex.com/v1/reference#rest-public-ticker
+        Docs: https://bitfinex.readme.io/v1/reference#rest-public-ticker
         """
-        symbol = params.pop('symbol')
-        endpoint = 'pubticker/' + symbol
+        endpoint = 'pubticker/' + params.pop('symbol')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def stats(self, **params):
         """ Various statistics about the requested pair.
         Docs: https://bitfinex.readme.io/v1/reference#rest-public-stats
         """
-        symbol = params.pop('symbol')
-        endpoint = 'stats/' + symbol
+        endpoint = 'stats/' + params.pop('symbol')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def fundingbook(self, **params):
         """ Get the full margin funding book.
         Docs: https://bitfinex.readme.io/v1/reference#rest-public-fundingbook
         """
-        symbol = params.pop('symbol')
-        endpoint = 'lendbook/' + symbol
+        endpoint = 'lendbook/' + params.pop('currency')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def orderbook(self, **params):
         """ Get the full order book.
         Docs: https://bitfinex.readme.io/v1/reference#rest-public-orderbook
         """
-        symbol = params.pop('symbol')
-        endpoint = 'book/' + symbol
+        endpoint = 'book/' + params.pop('symbol')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def trades(self, **params):
         """ Get a list of the most recent trades for the given symbol.
         Docs: https://bitfinex.readme.io/v1/reference#rest-public-trades
         """
-        symbol = params.pop('symbol')
-        endpoint = 'trades/' + symbol
+        endpoint = 'trades/' + params.pop('symbol')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def lends(self, **params):
         """ Get a list of the most recent funding data for the given currency: total amount lent and Flash Return Rate (in % by 365 days) over time.
         Docs: https://bitfinex.readme.io/v1/reference#rest-public-lends
         """
-        symbol = params.pop('symbol')
-        endpoint = 'lends/' + symbol
+        endpoint = 'lends/' + params.pop('currency')
         return self.request(endpoint, method='GET', auth=False, params=params)
 
     def symbols(self, **params):
